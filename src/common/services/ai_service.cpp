af2feb018e990b8b8109ab800b79f74a9abceb08
Victor Henrique <victor.henrique5800@gmail.com>
2023-05-31 16:01:18 -0300

    Encriptando chave de api

--- src/common/services/ai_service.cpp
+++ src/common/services/ai_service.cpp
@@ -4,7 +4,7 @@
 AIService::AIService(string base_prompt) {
     this->base_prompt = base_prompt;
     this-> history = vector<string>();
-    this-> api_key = "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"; // Victoria
+    this-> api_key = "";
     this-> model = "gpt-3.5-turbo";
     // this-> model = "text-davinci-003";
     this-> max_tokens = "100";
