a38e42e4b8de0bd78f3da15414301a3bc3dfa4c5
Victor Henrique <victor.henrique5800@gmail.com>
2023-06-12 12:37:31 -0300

    Adicionado e testado setContent(vector<string>)

--- src/common/database/player.cpp
+++ src/common/database/player.cpp
@@ -14,6 +14,7 @@ Player::Player(unsigned int id, string name){
         throw InvalidNameException();
     }
     this->name = name;
+    this->is_playing = true;
 }
 
 unsigned int Player::getId(){
@@ -23,3 +24,12 @@ unsigned int Player::getId(){
 string Player::getName(){
     return this->name;
 }
+
+bool Player::isPlaying(){
+    return this->is_playing;
+}
+
+void Player::eliminate(){
+    this->is_playing = false;
+}
+
