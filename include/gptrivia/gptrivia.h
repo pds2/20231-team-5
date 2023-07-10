a170f7dba102385065022eac5723f60d26ade1d3
Wesley Marques <wesleymarquesdc@gmail.com>
2023-07-10 00:39:12 -0300

    atualizar gptrivia.h

--- include/gptrivia/gptrivia.h
+++ include/gptrivia/gptrivia.h
@@ -1,3 +1,8 @@
+/**
+ * @file gptrivia.h
+ * @brief Declaração da classe GPTrivia.
+ */
+
 #ifndef GPTRIVIA_H
 #define GPTRIVIA_H
 
@@ -5,19 +10,30 @@
 using std::string;
 
 #include "../../include/common/services/ai_service.h"
+#include "../../include/common/services/event_service.h"
 #include "../../include/common/game.h"
 #include "database/dataloader.h"
 #include "service/cardservice.h"
 #include "service/viewtrivia.h"
 
+/**
+ * @brief Classe que representa o jogo de trivia.
+ */
 class GPTrivia : public Game{
 
   public:
+    /**
+     * @brief Construtor da classe GPTrivia.
+     */
     GPTrivia();
+
+    /**
+     * @brief Inicia o jogo.
+     */
     void playGame() override;
 
   private:
-    static const unsigned int numberOfRounds = 1; // COLOCAR 10
+    static const unsigned int numberOfRounds = 0;
     unsigned int numRound;
 
     DataLoader dataLoader;
@@ -25,13 +41,43 @@ class GPTrivia : public Game{
     CardService cardService;
     PlayerService playerService;
     ViewTrivia viewTrivia;
+    //EventService eventService;
 
-    void singlePlayer() override;
-    void multiPlayer() override;
-    void round(ScoreboardService& scoreboardService) override;
-    void processPlayerTurn(ScoreboardService& scoreboardService, const string header) override;
-    void resetData() override;
+    /**
+     * @brief Executa as rodadas do jogo.
+     */
+    void runGameRounds();
+
+    /**
+     * @brief Executa uma rodada do jogo.
+     * @param scoreboardService O serviço de pontuação.
+     */
+    void round(ScoreboardService& scoreboardService);
+
+    /**
+     * @brief Realiza a vez do jogador durante uma rodada.
+     * @param scoreboardService O serviço de pontuação.
+     * @param header O cabeçalho a ser exibido durante a vez do jogador.
+     */
+    void playerTurn(ScoreboardService& scoreboardService, const string header);
+
+    /**
+     * @brief Atualiza a pontuação do jogador com base no tempo de resposta e na correção da resposta.
+     * @param userTime O tempo de resposta do jogador.
+     * @param scoreboardService O serviço de pontuação.
+     * @param isCorrectAnswer Flag indicando se a resposta do jogador está correta.
+     */
     void updateScore(const unsigned int userTime, ScoreboardService& scoreboardService, const bool isCorrectAnswer);
+
+    /**
+     * @brief Reinicia os dados do jogo para uma nova partida.
+     */
+    void resetData();
+
+    /**
+     * @brief Obtém o cabeçalho para exibição durante uma rodada.
+     * @return O cabeçalho.
+     */
     string getHeader();
 
 };
