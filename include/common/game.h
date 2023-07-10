ddf2de035949f0378ec03b161a8091833412b441
Wesley Marques <wesleymarquesdc@gmail.com>
2023-07-10 02:00:40 -0300

    atualizar game.h

--- include/common/game.h
+++ include/common/game.h
@@ -1,11 +1,22 @@
+/**
+ * @file game.h
+ * @brief Declaração da classe Game.
+ */
+
 #ifndef GAME_H
 #define GAME_H
 
 #include "services/scoreboard_service.h"
 
+/**
+ * @brief Classe abstrata que representa um jogo.
+ */
 class Game{
 
   public:
+    /**
+     * @brief Executa o jogo.
+     */
     virtual void playGame() = 0;
 
 };
