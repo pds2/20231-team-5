1097167f37f17e27af4c4c498a326390b836b72b
Wesley Marques <wesleymarquesdc@gmail.com>
2023-07-10 00:50:10 -0300

    atualizar gperfil.h

--- include/gperfil/gperfil.h
+++ include/gperfil/gperfil.h
@@ -1,37 +1,49 @@
-
 #ifndef GPERFIL_H
 #define GPERFIL_H
 
-#include "service/rodada.h"
-#include "../common/services/player_service.h"
-#include "../common/services/scoreboard_service.h"
-#include "../../include/gperfil/service/viewperfil.h"
+#include "../../include/common/game.h"
 
-const int PONT_RODADA = 20;
-const int QNT_RODADA = 2;
+class GPerfil : public Game{
+  
+  public: 
+    GPerfil();
+    void playGame() override;
 
-class Gperfil
-{
-
-private:
-    PlayerService _playerservice;
-
-    ViewPerfil _view;
-
-    void vez_do_jogador(Rodada &x, ScoreboardService &scoreboardService,std::string tema, std::string objeto);
-
-    void resposta_do_jogador(Rodada &x, Player &currentPlayer, ScoreboardService &scoreboardService, std::string objeto);
-
-    bool continuar = true;
-
-public:
-    void Rodada_p(ScoreboardService &scoreboardService);
-
-    void Gperfil_jogo();
-
-    void iniciarview();
-
-    Gperfil();
 };
 
-#endif
+// #ifndef GPERFIL_H
+// #define GPERFIL_H
+
+// #include "service/rodada.h"
+// #include "../common/services/player_service.h"
+// #include "../common/services/scoreboard_service.h"
+// #include "../../include/gperfil/service/viewperfil.h"
+
+// const int PONT_RODADA = 20;
+// const int QNT_RODADA = 2;
+
+// class Gperfil
+// {
+
+// private:
+//     PlayerService _playerservice;
+
+//     ViewPerfil _view;
+
+//     void vez_do_jogador(Rodada &x, ScoreboardService &scoreboardService,std::string tema, std::string objeto);
+
+//     void resposta_do_jogador(Rodada &x, Player &currentPlayer, ScoreboardService &scoreboardService, std::string objeto);
+
+//     bool continuar = true;
+
+// public:
+//     void Rodada_p(ScoreboardService &scoreboardService);
+
+//     void Gperfil_jogo();
+
+//     void iniciarview();
+
+//     Gperfil();
+// };
+
+// #endif
